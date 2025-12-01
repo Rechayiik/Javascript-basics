43489eaff566b9ebd3f5dda8fd60ba46563ccb53
Rechayiik <Rechmajoda@gmail.com>
2025-12-01 21:37:41 +0300

    exercise work update

--- Index.js
+++ Index.js
@@ -1,1 +1,84 @@
-//Declaring variables in javascript 
+/* Declaring variables in javascript 
+*This is the first assignment of javascript class
+*/
+
+// ******************************* Question one **************************************** 
+
+
+//Single line comment are comments with double forward slashes like this line
+
+/*
+* This  is a multi line comment 
+* it spans more than one line 
+*/
+
+
+
+// ******************************** Question two ***************************************
+// Variable declaration with let and logging them to the console
+let me = "Rech";
+let score = 24;
+
+console.log(me);
+console.log(score);
+
+
+
+// ******************************** Questoin three ***************************************
+// variable declaration using const(constants)
+const pi = 22/7;
+console.log(pi);        console.log(`\n`);
+
+
+
+
+// ******************************** Questoin four ***************************************
+// checking datatypes of data given above
+let data = "Ayiik";
+let years = 5;
+let user;
+let id = null;
+let active = true;
+console.log(typeof(data));
+console.log(typeof(years));
+console.log(typeof(id));
+console.log(typeof(user));
+console.log(typeof(active));
+
+
+
+// ******************************** Questoin five ***************************************
+let name = "Rech";
+let age = 24;
+
+console.log(`My name is `, name);
+console.log(`and I am `, age, `years old \n`);
+
+
+
+// ******************************** Questoin six ***************************************
+// arithematic operation of +-*/ **
+console.log(6 + 4); //addition
+console.log(23 - 5); //subtraction
+console.log(3 * 5); //multiplication
+console.log(50 / 5); //division
+console.log(5 ** 2); //exponent/power 
+console.log(15 - (2 + 7)); //brackets
+
+
+
+
+// ******************************** Questoin seven ***************************************
+// bolean datatypes
+let on = true;
+let playing = false;
+console.log(on == true);
+console.log(playing == true);
+
+
+
+// ******************************** Questoin eight ***************************************
+// undefined and null values 
+
+let person1;            // this is undefined
+
